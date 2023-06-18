a3984d1fc54193facb6cd511d3c8e82bc7fb635e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-06-18 16:06:47 +0800

    feat: add edit project functionality and api-route (#6)

--- server/api/projects/index.ts
+++ server/api/projects/index.ts
@@ -1,4 +1,4 @@
-import prisma from "../../../composables/prisma";
+import prisma from "~/composables/prisma";
 
 export default defineEventHandler(async (event) => {
   const projects = await prisma.projects.findMany()
