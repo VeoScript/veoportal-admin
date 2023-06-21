b491651fcbe2082f1aff299702d163ff6254b680
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-06-21 20:26:28 +0800

    chore: turn off ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ssr: false,
+  ssr: false,
   devtools: { enabled: false },
   css: ['~/assets/css/tailwind.css'],
   postcss: {
