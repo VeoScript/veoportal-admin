def18c68064511f38b417be79a7d126dd663f37f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-06-26 16:29:10 +0800

    chore: update delete project and blog modal backdrop

--- components/DeleteProjectModal.vue
+++ components/DeleteProjectModal.vue
@@ -38,7 +38,11 @@ const handleDeleteProject = async () => {
 </script>
 
 <template>
-  <div v-if="isOpen" class="absolute inset-0 z-10 flex flex-col items-center justify-center w-full h-full backdrop-blur-xl bg-black bg-opacity-50">
+  <div v-if="isOpen" class="fixed inset-0 z-10 flex flex-col items-center justify-center w-full h-full backdrop-blur-xl bg-black bg-opacity-50">
+    <button
+      class="absolute flex-1 w-full h-full outline-none cursor-default bg-transparent"
+      v-on:click="() => isOpen ? setIsOpen(false) : null"
+    />
     <div class="z-20 flex flex-col items-center w-full max-w-xl p-10 space-y-10 rounded-xl border border-red-500 bg-accent-3">
       <p>Are you sure you want to delete this project <span class="font-bold">{{ title }}</span>?</p>
       <div class="flex flex-row items-center justify-center w-full space-x-1">
