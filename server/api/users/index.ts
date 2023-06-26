7c88a046f4f900ccdfa27bc09eeec24ab92c71dc
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-06-26 15:57:29 +0800

    chore: update order by to descending
    Update order by to descending for Projects, Blogs, and Admin Users

--- server/api/users/index.ts
+++ server/api/users/index.ts
@@ -1,6 +1,10 @@
 import prisma from "~/composables/prisma";
 
 export default defineEventHandler(async (event) => {
-  const users = await prisma.user.findMany()
+  const users = await prisma.user.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
   return users
 })
