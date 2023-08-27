4a40fe4d03a496efeace85636978f6dd5357a781
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-08-27 13:21:46 +0800

    chore: fix image upload in edit-account page (#23)

--- pages/edit-account.vue
+++ pages/edit-account.vue
@@ -83,7 +83,7 @@ const handleCreateProject = async (e: Event) => {
   try {
     let photo: string = ''
 
-    if (currenctImage.value === '') {
+    if (imageToUpload.value) {
       const body = new FormData()
       body.append('image', imageToUpload.value)
 
@@ -197,7 +197,7 @@ const handleChangePassword = async (e: Event) => {
       bodyClassName: "font-poppins font-light text-sm text-red-500",
       hideProgressBar: true,
     });
-    isLoading.value = false
+    isLoadingChangePassword.value = false
     return
   } else {
     toast.dark("Password updated successfully.", {
@@ -224,7 +224,7 @@ useHead({
 
 <template>
   <NuxtLayout>
-    <div class="flex-1 w-full overflow-y-auto">
+    <div v-if="!isPendingUser" class="flex-1 w-full overflow-y-auto">
       <TopBar title="Edit Account" />
       <LoaderSubmit
         v-if="isLoading"
